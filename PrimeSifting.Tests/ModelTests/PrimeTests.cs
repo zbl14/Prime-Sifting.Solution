b80a631f15ad1618398546dbf190e1e5c772660a
zbl14 <ifthereisoneday@gmail.com>
2022-07-13 12:04:57 -0700

    add Prime constructor, IsPrime method, tests passed

--- PrimeSifting.Tests/ModelTests/PrimeTests.cs
+++ PrimeSifting.Tests/ModelTests/PrimeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PrimeSifting.Models;
 
@@ -13,10 +14,46 @@ namespace PrimeSifting.Tests
     // }
 
     [TestMethod]
-    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
+    public void PrimeConstructor_CreatesInstanceOfPrime_Prime()
     {
-      // any necessary logic to prep for test; instantiating new classes, etc.
-      Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+      Prime newPrime = new Prime(2);
+      Assert.AreEqual(typeof(Prime), newPrime.GetType());
+    }
+
+    [TestMethod]
+    public void GetNum_ReturnsNum_Int()
+    {
+      int num = 2;
+      Prime newPrime = new Prime(num);
+      int primeNum = newPrime.Num;
+      Assert.AreEqual(num, primeNum);
+    }
+
+    [TestMethod]
+    public void PrimeTest_IsPrime_2IsTrue()
+    {
+      int num = 2;
+      Prime newPrime = new Prime(num);
+      int primeNum = newPrime.Num;
+      Assert.AreEqual(true, newPrime.IsPrime(num));
+    }
+
+    [TestMethod]
+    public void PrimeTest_IsPrime_True()
+    {
+      int num = 3;
+      Prime newPrime = new Prime(num);
+      int primeNum = newPrime.Num;
+      Assert.AreEqual(true, newPrime.IsPrime(num));
+    }
+
+    [TestMethod]
+    public void PrimeTest_IsPrime_False()
+    {
+      int num = 4;
+      Prime newPrime = new Prime(num);
+      int primeNum = newPrime.Num;
+      Assert.AreEqual(false, newPrime.IsPrime(num));
     }
   }
 }
--- PrimeSifting/Models/Prime.cs
+++ PrimeSifting/Models/Prime.cs
@@ -1,7 +1,32 @@
+using System.Collections.Generic;
+
 namespace PrimeSifting.Models
 {
   public class Prime
   {
-    // properties, constructors, methods, etc. go here
+    public int Num { get; set; }
+    private static List<Prime> _instances = new List<Prime> {};
+
+    public Prime(int num)
+    {
+      Num = num;
+      _instances.Add(this);
+    }
+
+    public bool IsPrime(int num)
+    {
+      for (int i = 2; i < num; i++)
+      {
+        if (num % i != 0)
+        {
+          i++;
+        }
+        else 
+        {
+        return false;
+        }
+      }
+      return true; 
+    }
   }
 }
