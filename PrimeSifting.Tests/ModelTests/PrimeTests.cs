a36a5b173787632ef185520cd66db070f604fb33
zbl14 <ifthereisoneday@gmail.com>
2022-07-13 13:37:51 -0700

    add GetAll methot for add primes to the list
    Co-authored-by: Tyler Emmerson <tyleremmerson0216@gmail.com>
    Co-authored-by: Justin Peters <JMarvel525@gmail.com>

--- PrimeSifting.Tests/ModelTests/PrimeTests.cs
+++ PrimeSifting.Tests/ModelTests/PrimeTests.cs
@@ -6,12 +6,12 @@ using PrimeSifting.Models;
 namespace PrimeSifting.Tests
 {
   [TestClass]
-  public class PrimeTests // : IDisposable
+  public class PrimeTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   ClassName.ClearAll();
-    // }
+    public void Dispose()
+    {
+      Prime.ClearAll();
+    }
 
     [TestMethod]
     public void PrimeConstructor_CreatesInstanceOfPrime_Prime()
@@ -55,5 +55,35 @@ namespace PrimeSifting.Tests
       int primeNum = newPrime.Num;
       Assert.AreEqual(false, newPrime.IsPrime(num));
     }
+
+    [TestMethod]
+    public void PrimeTest_ReturnPrimes_PrimeList()
+    {
+      int num1 = 2;
+      int num2 = 3;
+      Prime newPrime1 = new Prime(num1);
+      Prime newPrime2 = new Prime(num2);
+      List<Prime> newList = new List<Prime> {newPrime1, newPrime2};
+      List<Prime> result = Prime.GetAll(); 
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    // [TestMethod]
+    // public void PrimeTest_IsPrimeSOE_2IsTrue()
+    // {
+    //   int num = 2;
+    //   Prime newPrime = new Prime(num);
+    //   int primeNum = newPrime.Num;
+    //   Assert.AreEqual(true, newPrime.IsPrimeSOE(num));
+    // }
+
+    // [TestMethod]
+    // public void PrimeTest_IsPrimeSOE_False()
+    // {
+    //   int num = 4;
+    //   Prime newPrime = new Prime(num);
+    //   int primeNum = newPrime.Num;
+    //   Assert.AreEqual(false, newPrime.IsPrimeSOE(num));
+    // }
   }
 }
--- PrimeSifting/Models/Prime.cs
+++ PrimeSifting/Models/Prime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace PrimeSifting.Models
@@ -28,5 +29,36 @@ namespace PrimeSifting.Models
       }
       return true; 
     }
+
+    public static List<Prime> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    // public bool IsPrimeSOE(int num)
+    // { 
+    //   for (int i = 2; i < Math.Sqrt(num) + 1; i++)
+    //   {
+    //     bool prime = true;
+    //     if (prime) 
+    //     {
+    //       for (int j = (int) Math.Pow(i,2); j < num; j+=i) 
+    //       {
+    //         prime = false;
+    //       }
+    //       return prime;
+    //     }
+    //     else
+    //     {
+    //       return false;
+    //     }  
+    //   }
+    //   return true;
+    // }
   }
 }
